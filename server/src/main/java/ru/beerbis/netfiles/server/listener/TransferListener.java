37c09d5e2c9f0ba23a770d75670fe5aa5651cfd5
Виктор Елсуков <beerbis@yandex.ru>
2021-04-04 21:24:01 +0300

    feat: добавить логирования открытия/закрытия, закрытие threadPool-ов и закрытие слушателя по финализации try-"с ресурсами"

--- server/src/main/java/ru/beerbis/netfiles/server/listener/TransferListener.java
+++ server/src/main/java/ru/beerbis/netfiles/server/listener/TransferListener.java
@@ -4,23 +4,32 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.beerbis.netfiles.server.settings.ListenerSettings;
 
 import javax.annotation.Nonnull;
 
-public class TransferListener {
+public class TransferListener implements AutoCloseable {
+    private final Logger logger = LogManager.getLogger(TransferListener.class);
     protected final NioEventLoopGroup parentGroup = new NioEventLoopGroup(1);
     protected final NioEventLoopGroup childGroup = new NioEventLoopGroup();
     protected final ServerBootstrap listener = new ServerBootstrap();
+    protected ChannelFuture bindingFuture = null;
+    private final String name;
 
-    public TransferListener() {
+    public TransferListener(String name) {
+        this.name = name;
         listener.group(parentGroup, childGroup)
                 .option(ChannelOption.SO_BACKLOG, 1)
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<Channel>() {
+                .childHandler(new ChannelInitializer<>() {
                     @Override
-                    protected void initChannel(Channel ch) throws Exception {
+                    protected void initChannel(Channel ch) {
                         ch.pipeline();
                     }
                 });
@@ -28,6 +37,47 @@ public class TransferListener {
 
     @Nonnull
     public ChannelFuture open(@Nonnull ListenerSettings settings) {
-        return listener.bind(settings.getPort());
+        logger.info("{} gonna binding on port {}...", name, settings.getPort());
+        bindingFuture = listener.bind(settings.getPort())
+                .addListener(onBindFinished(settings.getPort()));
+
+        bindingFuture.channel().closeFuture()
+                .addListener(this::onListenerClosed);
+
+        return bindingFuture;
+    }
+
+    private GenericFutureListener<? extends Future<? super Void>> onBindFinished(Integer port) {
+        return future -> {
+            if (future.cause() != null) {
+                logger.error(name + " did not bound on port " + port, future.cause());
+            } else {
+                logger.info("{} bound on port {}", name, port);
+            }
+        };
+    }
+
+    private void onListenerClosed(Future<?> closingFuture) {
+        logger.info(name + " closed for incoming");
+
+        logger.info("{} closing incoming registration threads...", name);
+        parentGroup.shutdownGracefully();
+        logger.info("{} closed incoming registration threads", name);
+
+        logger.info("{} closing incoming processing threads...", name);
+        childGroup.shutdownGracefully();
+        logger.info("{} closed incoming processing threads", name);
+    }
+
+    @Override
+    public void close() throws InterruptedException {
+        if (bindingFuture == null) {
+            logger.info("{} close attempted, noting to close", name);
+        } else if (bindingFuture.cause() != null) {
+            logger.info("{} close attempted, binding was broken", name);
+        } else {
+            logger.info("{} gonna closing...", name);
+            bindingFuture.channel().close().sync();
+        }
     }
 }
--- server/src/test/java/ru/beerbis/netfiles/server/TransferListenerTest.java
+++ server/src/test/java/ru/beerbis/netfiles/server/TransferListenerTest.java
@@ -11,36 +11,48 @@ import static ru.beerbis.netfiles.server.settings.Settings.SETTINGS;
 public class TransferListenerTest {
 
     @Test
+    public void should_zeroActivityTest_success() throws InterruptedException {
+        try (var transfers = new TransferListener("transfers")) {
+        }
+    }
+
+    /**
+     * Интересный случай. Как бы и работает норм, но вроде бы и некорректно как-то.
+     * Как вариант: можно на `close` подождать завершения binding-фьючера, но зачем... Всё равно же закрываться.
+     * Как правильно поступать - не совсем очевидно.
+     * @throws InterruptedException ожидание фьючера закрытия(в {@link AutoCloseable} - sync) может быть прервано
+     */
+    @Test
+    public void should_openSynclessAndImmediatelyClose_success() throws InterruptedException {
+        try (var transfers = new TransferListener("transfers")) {
+            transfers.open(SETTINGS.getTransfer());
+        }
+    }
+
+    @Test
     public void should_openAndCloseOnce_success() throws InterruptedException {
-        var future = new TransferListener().open(SETTINGS.getTransfer());
-        future.sync();
-        future.channel().close().sync();
+        try (var transfers = new TransferListener("transfers")) {
+            transfers.open(SETTINGS.getTransfer()).sync();
+        }
     }
 
     @Test
     public void should_openAndCloseTwice_success() throws InterruptedException {
-        var future1 = new TransferListener().open(SETTINGS.getTransfer());
-        future1.sync();
-        future1.channel().close().sync();
+        try (var transfers = new TransferListener("transfers1")) {
+            transfers.open(SETTINGS.getTransfer()).sync();
+        }
 
-        var future2 = new TransferListener().open(SETTINGS.getTransfer());
-        future2.sync();
-        future2.channel().close().sync();
+        try (var transfers = new TransferListener("transfers2")) {
+            transfers.open(SETTINGS.getTransfer()).sync();
+        }
     }
 
     @Test(expected = BindException.class)
     public void should_openWithAlreadyOpened_bindingFail() throws InterruptedException {
-        ChannelFuture future1 = null;
-        ChannelFuture future2;
-        try {
-            future1 = new TransferListener().open(SETTINGS.getTransfer());
-            future1.sync();
-
-            future2 = new TransferListener().open(SETTINGS.getTransfer());
-            future2.sync();
-            future2.channel().close().sync();
-        } finally {
-            future1.channel().close().sync();
+        try (var transfers1 = new TransferListener("transfers1");
+             var transfers2 = new TransferListener("transfers2")) {
+            transfers1.open(SETTINGS.getTransfer()).sync();
+            transfers2.open(SETTINGS.getTransfer()).sync();
         }
     }
 }
